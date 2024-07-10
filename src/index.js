7dc17f3d0efb39710e696c00e1be41d739098bef
Daniyalahmadsaeedantechpvt <daniyalahmad@saeedantechpvt.com>
2024-07-10 15:34:13 +0500

    App page utils are added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-connectDB();
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MONGO db connection failed !!!", err));
